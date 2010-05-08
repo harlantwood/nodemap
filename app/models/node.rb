4e457e9d9a533ef9813047e89094591b56a9df94
Harlan Knight Wood <warrior@thegoldensun.com>
2010-05-08 16:35:47 -0700

    rearrange lines for readability

--- app/models/node.rb
+++ app/models/node.rb
@@ -3,9 +3,9 @@ class Node < ActiveRecord::Base
   has_many :relationships
   has_many :related_nodes, :through => :relationships
   
-  validates_presence_of :key
-  validates_presence_of :html
+  validates_presence_of   :key
   validates_uniqueness_of :key
+  validates_presence_of   :html
   
   def to_param
     URI::escape( key )
